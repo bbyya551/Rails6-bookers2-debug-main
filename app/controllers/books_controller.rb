0160e88b4d3bf5ce4430ed6a81eeca43f98aa9eb
bbyya551 <106591130+bbyya551@users.noreply.github.com>
2022-06-12 09:41:32 +0000

    [Add] reiaut edit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,7 +5,6 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @user = @book.user
     @book_comment = BookComment.new
-    @favorites = Favorite.all
   end
 
   def index
