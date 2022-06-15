10564b570ecc01e84964f3e1ab4d12fa9231c096
bbyya551 <106591130+bbyya551@users.noreply.github.com>
2022-06-15 08:37:07 +0000

    [Update] search function syuusei

--- app/models/book.rb
+++ app/models/book.rb
@@ -9,17 +9,15 @@ class Book < ApplicationRecord
     favorites.exists?(user_id: user.id)
   end
 
-  def self.looks(search, word)
-    if search == "perfect_match"
-      @book = Book.where("title LIKE?", "#{word}")
-    elsif search == "forward_match"
-      @book = Book.where("title LIKE?", "#{word}%")
-    elsif search == "backward_match"
-      @book = Book.where("title LILE?", "%#{word}")
-    elsif search == "partial_match"
-      @book = Book.where("title LIKE?", "%#{word}%")
-    else
-      @book = Book.all
+  def self.search_for(content, method)
+    if method == 'perfect'
+      Book.where(title: content)
+    elsif method == 'forward'
+      Book.where('title LIKE ?', content + '%')
+    elsif method == 'backward'
+      Book.where('title LIKE ?', '%' + content)
+    elsif method == 'partial'
+      Book.where('title LIKE ?', '%' + content + '%')
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -36,17 +36,15 @@ class User < ApplicationRecord
     reverse_of_relationships.find_by(follower_id: user.id).present?
   end
 
-  def self.looks(search, word)
-    if search == "perfect_match"
-      @user = User.where("name LIKE?", "#{word}")
-    elsif search == "forward_match"
-      @user = User.where("name LIKE?", "#{word}%")
-    elsif search == "backward_match"
-      @user = User.where("name LIKE?", "%#{word}")
-    elsif search == "partial_match"
-      @user = User.where("name LIKE?", "%#{word}%")
-    else
-      @user = User.all
+  def self.search_for(content, method)
+    if method == 'perfect'
+      User.where(name: content)
+    elsif method == 'forward'
+      User.where('name LIKE ?', content + '%')
+    elsif method == 'backward'
+      User.where('name LIKE ?', '%' + content)
+    elsif method == 'partial'
+      User.where('name LIKE ?', '%' + content + '%')
     end
   end
 end
